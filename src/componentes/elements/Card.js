7943f9e5df30eba27622a71bd8ab6621ef52ccc6
João Augusto <131208157+JohnJA12@users.noreply.github.com>
2024-04-23 16:20:26 -0300

    Update Card.js

--- src/componentes/elements/Card.js
+++ src/componentes/elements/Card.js
@@ -1,18 +1,7 @@
 import styes from './Card.module.css'
-import { useState } from 'react'
 import ButttonB from './ButtonB'
 
 function Card({img,title,tech, description, repo, site}){
-
-     const [info,setInfo] = useState(false)
-
-    function InfoOn(){
-        setInfo(true)
-    }
-
-    function InfoOff(){
-        setInfo(false)
-    }
     
     return(
         <div className={styes.card}>
@@ -20,14 +9,13 @@ function Card({img,title,tech, description, repo, site}){
                 <img src={img} alt='ERROR'/>
             </a>
 
-         {info == true &&(
+        
             <section>
                 <h3>{title}</h3>
                 <p><strong> Tecnologia: </strong> {tech}</p>
                 <p> {description}</p>
                  <ButttonB text='Acesse o repositório' link={repo}/>
             </section>
-             )}
         </div>
     )
 } 
