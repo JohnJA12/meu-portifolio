f59584ef74eec3778ef6298976b4d7d062c667e5
João Augusto <131208157+JohnJA12@users.noreply.github.com>
2024-04-23 16:14:18 -0300

    Update Presentation.js

--- src/componentes/section/Presentation.js
+++ src/componentes/section/Presentation.js
@@ -1,7 +1,41 @@
 import styles from './Presentation.module.css'
 import ButtonA from '../elements/ButtonA';
-import ButttonB from '../elements/ButtonB';
+import {useEffect, useState} from 'react';
 function Presentation(){
+
+const [text,setText] = useState('');
+    const toRotate = ['João Augusto', 'Desenvolvedor Fullstack', 'Estudante de Tecnologia'];
+    const [loop, setLoop] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const period = 100;
+    const [delta, setDelta] = useState(100)
+
+    useEffect(()=>{
+        let ticker = setInterval(()=>{
+            toType()
+        },delta)
+        return()=> {clearInterval(ticker)}
+
+    }, [text])
+
+    const toType = () => {
+        let i = loop % toRotate.length;
+        let fullText = toRotate[i]
+        let updatedText = isDeleting? fullText.substring(0,text.length-1) : fullText.substring(0,text.length+1)
+        
+        setText(updatedText);
+
+        if(!isDeleting && updatedText === fullText ){
+         setIsDeleting(true);
+         setDelta(period)
+        } else if(isDeleting && updatedText === ''){
+            setIsDeleting(false);
+            setDelta(period);
+            setLoop(loop+1);
+        }
+    
+
+    
     return(
         <div id='Presentation'  className={styles.Presentation}>
             <h4><strong>Bem-vindo ao meu Portfólio</strong></h4>
